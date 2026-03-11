82bc7fb251d3878e9de76e0bc162f296c55ca5b4
KIRNKUMR-7 <kiran2006bgkr@gmail.com>
2026-03-11 22:04:26 +0530

    Replaced repo files with local files

--- backend/src/main/java/com/example/CourseRegister/Repository/CourseRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.CourseRegister.Repository;
-
-import com.example.CourseRegister.Module.Course;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CourseRepository extends JpaRepository<Course,Integer> {
-}
